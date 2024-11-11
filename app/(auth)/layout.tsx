9690f695bca5e3a6416c9d12897b3a2f210a71f6
Farok Hossain <farokafs7@gmail.com>
2024-11-11 23:55:50 +0600

    updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -17,7 +17,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <div className="space-y-5 text-white">
             <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
-              This is a place where you cna store all your documents.
+              This is a place where you can store all your documents. It is the
+              best store you ever seen!!
             </p>
           </div>
         </div>
