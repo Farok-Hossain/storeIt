3f1d723b8bc857e058554771cc552db015f71e05
Farok Hossain <farokafs7@gmail.com>
2024-11-13 20:40:19 +0600

    updated

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -1,7 +1,8 @@
+"use client";
+
 import {
   AlertDialog,
   AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
@@ -12,12 +13,13 @@ import {
 import {
   InputOTP,
   InputOTPGroup,
-  InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Button } from "./ui/button";
+import { verifySecret } from "@/lib/actions/user.actions";
+import { useRouter } from "next/navigation";
 
 const OTPModal = ({
   accountId,
@@ -26,6 +28,7 @@ const OTPModal = ({
   accountId: string;
   email: string;
 }) => {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(true);
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -35,6 +38,8 @@ const OTPModal = ({
     setIsLoading(true);
 
     try {
+      const sessionId = await verifySecret({ accountId, password });
+      if (sessionId) router.push("/");
     } catch (error) {
       console.log("Failed to verify OTP", error);
     }
