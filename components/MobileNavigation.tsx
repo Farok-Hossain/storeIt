13458e5c39eba0628ca1db849ba50ecf5731b67f
Farok Hossain <farokafs7@gmail.com>
2024-11-14 12:12:47 +0600

    Header & MobileNavigation added

---
+++ components/MobileNavigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MobileNavigation = () => {
+  return (
+    <div>
+      <h1>Mobile Navigation</h1>
+    </div>
+  );
+};
+
+export default MobileNavigation;
+<h1>Mobile Navigation</h1>;
