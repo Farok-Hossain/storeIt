24389380abd5b9204d52e64ccc5a14de0f742276
Farok Hossain <farokafs7@gmail.com>
2024-11-16 23:50:56 +0600

    updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { avatarPlaceholderUrl, navItems } from "@/constants";
+import { navItems } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-const Sidebar = () => {
+interface Props {
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+
+const Sidebar = ({ fullName, avatar, email }: Props) => {
   const pathname = usePathname();
 
   return (
@@ -65,12 +71,16 @@ const Sidebar = () => {
 
       <div className="sidebar-user-info">
         <Image
-          src={avatarPlaceholderUrl}
+          src={avatar}
           alt="Avatar"
           width={44}
           height={44}
           className="sidebar-user-avatar"
         />
+        <div className="hidden lg:block">
+          <p className="subtitle-2 capitalize">{fullName}</p>
+          <p className="caption">{email}</p>
+        </div>
       </div>
     </aside>
   );
