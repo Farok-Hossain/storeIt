9e3119ead8234e9928c6f1896f49753efcd9fa91
Farok Hossain <farokafs7@gmail.com>
2024-11-18 18:00:54 +0600

    index.d.ts added & file.actions.ts updated

---
+++ lib/actions/file.actions.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { createAdminClient } from "../appwrite";
+import { InputFile } from "node-appwrite/file";
+import { appwriteConfig } from "../appwrite/config";
+import { ID } from "node-appwrite";
+import { constructFileUrl, getFileType } from "../utils";
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const upLoadFile = async ({
+  file,
+  ownerId,
+  accountId,
+  path,
+}: UploadFileProps) => {
+  const { storage, databases } = await createAdminClient();
+
+  try {
+    const inputFile = InputFile.fromBuffer(file, file.name);
+
+    const bucketFile = await storage.createFile(
+      appwriteConfig.bucketId,
+      ID.unique(),
+      inputFile
+    );
+
+    const fileDocument = {
+      type: getFileType(bucketFile.name).type,
+      name: bucketFile.name,
+      url: constructFileUrl(bucketFile.$id),
+      extension: getFileType(bucketFile.name).extension,
+      size: bucketFile.sizeOriginal,
+      owner: ownerId,
+      accountId,
+      users: [],
+      bucketFileId: bucketFile.$id,
+    };
+  } catch (error) {
+    handleError(error, "Failed to upload file");
+  }
+};
