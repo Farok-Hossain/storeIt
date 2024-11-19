2b0347e233c64dfa07cdffe3a901ca460e587d27
Farok Hossain <farokafs7@gmail.com>
2024-11-19 21:56:54 +0600

    page.tsx updated

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-const Page = () => {
+const Page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden sm:block text-light-200">Sort by:</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
